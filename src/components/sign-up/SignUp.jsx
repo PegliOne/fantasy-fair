eef5f10bd126749a1df09c9d42ca3afa42a54b8c
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-07-04 14:35:01 +1000

    Added user creation functionality

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import SignUpForm from "./SignUpForm";
 
 const SignUp = () => {
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
-  return ( 
+  return (
     <main>
       <h1>Sign Up</h1>
-      <SignUpForm username={username} setUsername={setUsername} email={email} setEmail={setEmail} password={password} setPassword={setPassword} />
+      <SignUpForm />
     </main>
   );
 }
--- src/components/sign-up/SignUpForm.jsx
+++ src/components/sign-up/SignUpForm.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 import { StyledForm } from "../shared/styles";
 import FormInput from "../shared/form-components/FormInput";
 
-const SignUpForm = (props) => {
-  return (<StyledForm>
+const SignUpForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  function handleSubmit (e) {
+    e.preventDefault();
+
+    const user = { username, email, password };
+
+    setIsPending(true);
+
+    fetch(`http://localhost:8000/users`, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user)
+    }).then(()=> {
+      setIsPending(false);
+      history.push('/');
+    });
+  }
+
+  return (<StyledForm onSubmit={(e) => handleSubmit(e)}>
     <div className="form-row">
-      <FormInput type="text" label="username" value={props.username} function={props.setUsername} />
+      <FormInput type="text" label="username" value={username} function={setUsername} />
     </div>
     <div className="form-row">
-      <FormInput type="text" label="email" value={props.email} function={props.setEmail} />
+      <FormInput type="email" label="email" value={email} function={setEmail} />
     </div>
     <div className="form-row">
-      <FormInput type="text" label="password" value={props.password} function={props.setPassword} />
+      <FormInput type="password" label="password" value={password} function={setPassword} />
     </div>
-    <button type="submit">Sign In</button>
+    <div>
+      { !isPending && <button type="submit">Sign Up</button> }
+      { isPending && <button type="submit">Creating User</button> }
+    </div>
   </StyledForm>);
 }
- 
+
 export default SignUpForm;
